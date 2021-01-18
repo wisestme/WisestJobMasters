21de9080b341169988899e1e24f3e02e97bf558a
wisestMe <wyhzest@gmail.com>
2021-01-18 16:25:12 +0100

    add migration

---
+++ WisestJobMasters/Migrations/202101181523135_PopulateApplicants.cs
@@ -0,0 +1,16 @@
+namespace WisestJobMasters.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateApplicants : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
