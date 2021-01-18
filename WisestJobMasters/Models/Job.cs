ac52233beeb39f11478aaf986d7c3de6bdebf0a8
wisestMe <wyhzest@gmail.com>
2021-01-18 09:17:32 +0100

    add job class

---
+++ WisestJobMasters/Models/Job.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WisestJobMasters.Models
+{
+    public class Job
+    {
+        public int JobId { get; set; }
+        public int CompanyId { get; set; }
+        public string JobTitle { get; set; }
+        public string JobCity { get; set; }
+        public string JobCategory { get; set; }
+        public DateTime VacancyDeadline { get; set; }
+        public bool VacancyStatus { get; set; }
+    }
+}
