d2a35244c958b19bf34f037d2ac22726b5326692
wisestMe <wyhzest@gmail.com>
2021-01-18 11:52:27 +0100

    add view model

---
+++ WisestJobMasters/ViewModels/RandomJobsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WisestJobMasters.Models;
+
+namespace WisestJobMasters.ViewModels
+{
+    public class RandomJobsViewModel
+    {
+        public Job Job { get; set; }
+        public List<Applicant> Applicants { get; set; }
+    }
+}
