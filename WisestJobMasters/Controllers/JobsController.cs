d844f8bddab4f6e40e5a6e36f2db2571ac501708
wisestMe <wyhzest@gmail.com>
2021-01-18 09:34:16 +0100

    add jobs controller

---
+++ WisestJobMasters/Controllers/JobsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WisestJobMasters.Models;
+
+namespace WisestJobMasters.Controllers
+{
+    public class JobsController : Controller
+    {
+        // GET: Jobs
+        public ActionResult Random()
+        {
+            var job = new Job() { JobTitle = "C# Programmer" };
+            return View();
+        }
+    }
+}
