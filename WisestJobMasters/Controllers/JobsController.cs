acb50f816263e3c109c3dbee0642873a5727441f
wisestMe <wyhzest@gmail.com>
2021-01-18 14:15:06 +0100

    pass info to view

--- WisestJobMasters/Controllers/JobsController.cs
+++ WisestJobMasters/Controllers/JobsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WisestJobMasters.Models;
+using WisestJobMasters.ViewModels;
 
 namespace WisestJobMasters.Controllers
 {
@@ -13,7 +14,19 @@ namespace WisestJobMasters.Controllers
         public ActionResult Random()
         {
             var job = new Job() { JobTitle = "C# Programmer", JobCategory = "Computer Software" };
-            return View(job);
+            var applicants = new List<Applicant>
+            {
+                new Applicant {FirstName = "Chijioke"},
+                new Applicant {FirstName = "Emeka"},
+                new Applicant {FirstName = "Chidiebube"}
+            };
+
+            var viewModel = new RandomJobsViewModel
+            {
+                Job = job,
+                Applicants = applicants
+            };
+            return View(viewModel);
         }
     }
 }
